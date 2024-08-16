5355b7e50b14108b2379586b8db9fd6b961cce58
Anish <anish.agclg@gmail.com>
2024-08-17 02:48:37 +0530

    Third commit

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -8,15 +8,25 @@
 import Foundation
 import MapKit
 
-struct Location{
+struct Location: Identifiable{
    
+    //random generated id for unique referencing
+//    let id = UUID().uuidString
     //basic DataModel
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
     let description: String
     let imageNames: [String]
-    let link: [String]
+    let link: String
     
+    //creating a manual id
+    //Identifiable
+    var id: String{
+        //name = "Colosseum"
+        //cityName "Rome"
+        //id = "ColosseumRom"
+        name + cityName
+    }
 }
 
