81d4bebec1898e69c52765db724b9d06fb8717b7
Anish <anish.agclg@gmail.com>
2024-08-17 02:20:10 +0530

    Added data model for locations

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -6,8 +6,17 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location{
-    
+   
+    //basic DataModel
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: [String]
     
 }
+
